11bcd3992f0bd5562e4739b8c536fe9c55f255d2
Atle Frenvik Sveen <atle.sveen@bouvet.no>
2014-09-02 10:43:55 +0200

    pep8 fixes

--- ssrSok/app.py
+++ ssrSok/app.py
@@ -6,7 +6,7 @@ from flask import request, Response
 import xmltodict
 import requests
 import json
-from collections import OrderedDict
+import urllib
 
 app = Flask(__name__)
 
@@ -14,62 +14,83 @@ app = Flask(__name__)
 addZoomValues = True
 
 # Dictionary holding navnetype and zoomlevel
-zoomValues = {u"By": 12, u"Kommune": 11, u"Fjellområde": 10, u"Verneområder": 10, u"Innsjø": 14}
+zoomValues = {
+    u"By": 12,
+    u"Kommune": 11,
+    u"Fjellområde": 10,
+    u"Verneområder": 10,
+    u"Innsjø": 14
+}
+
+base_url = "https://ws.geonorge.no/SKWS3Index/ssr/sok"
+
 
 @app.route('/')
 def home():
-	return "ssrJsonSearch"
+    return "ssrJsonSearch"
+
 
 @app.route('/ssr')
 def ssrSok():
     query = request.args.get('query', '')
-    nordLL = request.args.get('nordLL')
-    ostLL = request.args.get('ostLL')
-    nordUR = request.args.get('nordUR')
-    ostUR = request.args.get('ostUR')
+    nordLL = request.args.get('nordLL', None)
+    ostLL = request.args.get('ostLL', None)
+    nordUR = request.args.get('nordUR', None)
+    ostUR = request.args.get('ostUR', None)
 
     bbox = all([nordLL, ostLL, nordUR, ostUR])
 
-    if bbox:	
-    	print """https://ws.geonorge.no/SKWS3Index/ssr/sok?navn=""" + query + """*&nordLL=""" + nordLL + """&ostLL=""" + ostLL + """&nordUR=""" + nordUR + """&ostUR=""" + ostUR + """&antPerSide=9&epsgKode=4258&eksakteForst=true"""
-    	r = requests.get("""https://ws.geonorge.no/SKWS3Index/ssr/sok?navn=""" + query + """*&nordLL=""" + nordLL + """&ostLL=""" + ostLL + """&nordUR=""" + nordUR + """&ostUR=""" + ostUR + """&antPerSide=9&epsgKode=4258&eksakteForst=true""", verify=False)
-    else:
-        r = requests.get('https://ws.geonorge.no/SKWS3Index/ssr/sok?navn='+query+'*&antPerSide=9&epsgKode=4258&eksakteForst=true', verify=False)
+    query_params = {
+        "navn": query,
+        "antPerSide": 9,
+        "epsgKode": "4258",
+        "eksakteForst": "true"
+    }
+
+    if bbox:
+        query_params.update({
+            "nordLL": nordLL,
+            "ostLL": ostLL,
+            "nordUR": nordUR,
+            "ostUR": ostUR
+        })
+
+    url = base_url + "?" + urllib.urlencode(query_params)
+    print url
+    r = requests.get(url, verify=False)
     doc = xmltodict.parse(r.text)
-
     # Add zoom values
-    if addZoomValues: 
-	    i = 0 # counter
-	    try:
-	    	# Check to see if we only get one result
-	    	ssrId = doc["sokRes"]["stedsnavn"]["ssrId"]
-	    	stedsnavn = dict(doc["sokRes"]["stedsnavn"]) # convert to regular dict
-	    	try:
-	    		# Check if this navnetype exist in zoomValues
-	    		stedsnavn["zoom"] = zoomValues[stedsnavn["navnetype"]]
-	    	except KeyError:
-	    		# If not set to default zoom level 15
-	    		stedsnavn["zoom"] = 15
-	    	doc["sokRes"]["stedsnavn"] = [stedsnavn] # Always return array
-	    except TypeError:
-		    for x in doc["sokRes"]["stedsnavn"]: # every stedsnavn
-		    	stedsnavn = dict(x) # convert to regular dict
-		    	try:
-		    		# Check if this navnetype exist in zoomValues
-		    		stedsnavn["zoom"] = zoomValues[stedsnavn["navnetype"]]
-		    	except KeyError:
-		    		# If not set to default zoom level 15
-		    		stedsnavn["zoom"] = 15
-		    	doc["sokRes"]["stedsnavn"][i] = stedsnavn
-		    	i += 1
- 
-    js = json.dumps(doc)
+    if addZoomValues:
+        i = 0  # counter
+        try:
+            # Check to see if we only get one result
+            ssrId = doc["sokRes"]["stedsnavn"]["ssrId"]
 
-    resp = Response(js, status=200, mimetype='application/json')
+            # convert to regular dict
+            stedsnavn = dict(doc["sokRes"]["stedsnavn"])
+            try:
+                # Check if this navnetype exist in zoomValues
+                stedsnavn["zoom"] = zoomValues[stedsnavn["navnetype"]]
+            except KeyError:
+                # If not set to default zoom level 15
+                stedsnavn["zoom"] = 15
+            doc["sokRes"]["stedsnavn"] = [stedsnavn]  # Always return array
+        except TypeError:
+            for x in doc["sokRes"]["stedsnavn"]:  # every stedsnavn
+                stedsnavn = dict(x)  # convert to regular dict
+                try:
+                    # Check if this navnetype exist in zoomValues
+                    stedsnavn["zoom"] = zoomValues[stedsnavn["navnetype"]]
+                except KeyError:
+                    # If not set to default zoom level 15
+                    stedsnavn["zoom"] = 15
+                doc["sokRes"]["stedsnavn"][i] = stedsnavn
+                i += 1
+
+    resp = Response(json.dumps(doc), status=200, mimetype='application/json')
     return resp
+
 
 if __name__ == "__main__":
     app.debug = False
     app.run()
-
-
